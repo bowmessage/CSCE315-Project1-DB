505dc766f2d3c93413f6db1539c769134180443b
ajcampbell318 <ajcampbell318@gmail.com>
2013-09-06 16:29:02 -0500

    Update Relation.cpp

--- Relation.cpp
+++ Relation.cpp
@@ -1,14 +1,45 @@
 #include "Relation.h"
 
+using namespace std;
 
+//creates a relation of name "n"
+Relation::Relation(string n){
+		Name = n;
+}
+
+//removes a relation of name "n"
+Relation::~Relation(string n){
+	Name = NULL;
+	entity.erase();
+	attribute.erase();
+}
+
+//adds entity "e" to the relation of name "n"
+Entity Relation::addEntity(Entity e){
+	entity.push_back(e);
+	return e;
+}
+
+//removes entity "e" from relation "n"
+Entity Relation::removeEntity(Entity e){
+	for(int i = 0; i < entity.size(); i++){
+		if(e == entity[i]){
+			entity[i].erase();
+			return e;
+	}
+}
+
+//adds attribute "a" to the relation of name "n"
 Attribute Relation::addAttribute(Attribute a){
-  return a;
-}
-Attribute Relation::removeAttribute(Attribute a){
-  return a;
+	attribute.push_back(a);
+	return a;
 }
 
-bool Relation::operator==(const Relation &other){
-  //TODO implement this
-  return false;
+//adds entity "e" to the relation of name "n"
+Attribute Relation::removeAttribute(Attribute a){
+	for(int i = 0; i < r.attribute.size(); i++){
+		if(a == r.attribute[i]){ 
+			attribute[i].erase();
+			return a;
+	}
 }
