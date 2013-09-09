03e5b1fb82fdb5170e755f309726f196058814fb
ajcampbell318 <ajcampbell318@gmail.com>
2013-09-09 17:32:55 -0500

    Update DatabaseManager.h

--- DatabaseManager.h
+++ DatabaseManager.h
@@ -22,6 +22,8 @@ class DatabaseManager{
     bool createTable(string name, vector<Attribute> attributes, vector<Attribute*> keys);
     bool insertInto(string name, vector<string> literals);
     bool deleteRelation(string name);
+    void show(string name);
+	  void rename(vector<string> newNames, Relation r);
 
     vector< vector<string> >* select(string relationName, string op1, string comparison, string op2);
     vector< vector<string> >* project(string relationName, vector<string> attributeNames);
